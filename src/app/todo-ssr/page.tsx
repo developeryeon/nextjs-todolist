fd6596aff1b5d6996f8cbe580f52c0bdd82bccb4
yeon <test2@test2.com>
2024-03-15 10:01:16 +0900

    [add] : css 추가

--- src/app/todo-ssr/page.tsx
+++ src/app/todo-ssr/page.tsx
@@ -1,3 +1,4 @@
+import TodoForm from '@/components/todoComponents/TodoForm';
 import { TodoType } from '@/types';
 import Link from 'next/link';
 import React from 'react';
@@ -9,50 +10,54 @@ const TodoSSRPage = async () => {
 	const todoList: TodoType[] = await response.json();
 
 	return (
-		<section>
-			<div className="text-2xl">
-				<Link href={'/report'}>할일 정보 통계 보러가기</Link>
+		<section className="container mx-auto py-8">
+			<div className="text-center text-2xl mb-8">
+				<span className="text-blue-500 hover:underline">
+					<Link href={'/report'}>할일 정보 통계 보러가기</Link>
+				</span>
 			</div>
-			<hr />
-			<div className="bg-indigo-100 outline outline-offset-2  max-w-96">
-				<h2 className="text-3xl text-black ml-4 ">Working..!🔥</h2>
-				<div>
-					{todoList
-						.filter((todoItem) => {
-							return todoItem.isDone === false;
-						})
-						.map((todoItem) => (
-							<div key={todoItem.id}>
-								<p>
-									<label>제목: {todoItem.title}</label>
-								</p>
-								<p>
-									<label>내용: {todoItem.contents}</label>
-								</p>
-							</div>
-						))}
-				</div>
-			</div>
-			<br />
-			<div>
-				<h2 className="text-3xl text-black ml-4">Done..!🎉</h2>
-				<div>
-					{todoList
-						.filter((todoItem) => {
-							return todoItem.isDone === true;
-						})
-						.map((todoItem) => {
-							return (
-								<div key={todoItem.id}>
-									<p>
+
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+				<div className="bg-indigo-100 rounded-lg p-6">
+					<h2 className="text-3xl font-semibold mb-4">Working..!🔥</h2>
+					<div>
+						{todoList
+							.filter((todoItem) => {
+								return todoItem.isDone === false;
+							})
+							.map((todoItem) => (
+								<div key={todoItem.id} className="mb-4">
+									<p className="text-lg font-semibold">
 										<label>제목: {todoItem.title}</label>
 									</p>
-									<p>
+									<p className="text-sm">
 										<label>내용: {todoItem.contents}</label>
 									</p>
 								</div>
-							);
-						})}
+							))}
+					</div>
+				</div>
+
+				<div className="bg-green-100 rounded-lg p-6">
+					<h2 className="text-3xl font-semibold mb-4">Done..!🎉</h2>
+					<div>
+						{todoList
+							.filter((todoItem) => {
+								return todoItem.isDone === true;
+							})
+							.map((todoItem) => {
+								return (
+									<div key={todoItem.id} className="mb-4">
+										<p className="text-lg font-semibold">
+											<label>제목: {todoItem.title}</label>
+										</p>
+										<p className="text-sm">
+											<label>내용: {todoItem.contents}</label>
+										</p>
+									</div>
+								);
+							})}
+					</div>
 				</div>
 			</div>
 		</section>
